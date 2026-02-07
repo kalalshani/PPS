7cd48589b45d45bb35065eb2eb828c9a5c4bdaf5
Shani <shanikalal36@gmail.com>
2026-02-07 20:43:05 +0530

    Prectical 1.c
    Added arithmetic operations including addition, subtraction, multiplication, division, and remainder.

--- Prectical 1.c
+++ Prectical 1.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 int main()
 {
-  printf("Hi, my name is shani");
+  printf("\narethmatic oprations");
+  int a=27;
+  int b=17;
+  printf("addion is=%d",a + b );
+  
+  printf("subtraction is=%d",a - b );
+  
+  printf("multiplication is=%d",a * b );
+  
+  printf("divison is=%d",a / b );
+  
+  printf("reminder is=%d",a % b );
   return 0;
 }
